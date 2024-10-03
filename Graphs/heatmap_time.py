028ef921e946a1be8987f19b4efb89bbec287f83
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-10-03 18:58:08 +0300

    upload: plotly charts
    some example plotly chart applications

---
+++ Graphs/heatmap_time.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("Data/Historical NBA Performance.csv")
+df = df.pivot("Team","Year","Winning Percentage")
+print(df[:10])
+
+df = df[df.index.isin(["Warriors", "Knicks", "Celtics", "Bulls", "76ers"])]
+fig = px.imshow(df, color_continuous_scale=px.colors.sequential.YlOrBr,
+                title="NBA Season Winning Percentage")
+fig.update_layout(title_font={"size":27}, title_x=0.5)
+fig.update_traces(hoverongaps=False,
+                  hovertemplate="Team: %{y}"
+                                "<br>Year: %{x}"
+                                "<br>Winning %: %{z}<extra></extra>"
+                  )
+fig.show()
